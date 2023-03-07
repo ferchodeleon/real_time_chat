03b2e5ae9c949872d726f730723bd627e61b8aa1
Fernando De Leon <fdeleon@lacadenaretail.com>
2023-03-07 17:39:11 -0500

    correction return Map

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,13 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_real_time_chat/pages/chat_page.dart';
 import 'package:flutter_real_time_chat/pages/loading_page.dart';
 import 'package:flutter_real_time_chat/pages/login_page.dart';
 import 'package:flutter_real_time_chat/pages/register_page.dart';
 import 'package:flutter_real_time_chat/pages/users_page.dart';
 
-final Map<String, dynamic> appRoutes = {
-  'usuarios': (_) => UsersPage(),
-  'chat': (_) => ChatPage(),
-  'login': (_) => LoginPage(),
-  'register': (_) => RegisterPage(),
-  'loading': (_) => LoadingPage(),
+final Map<String, Widget Function(BuildContext)> appRoutes = {
+  'usuarios': (_) => const UsersPage(),
+  'chat': (_) => const ChatPage(),
+  'login': (_) => const LoginPage(),
+  'register': (_) => const RegisterPage(),
+  'loading': (_) => const LoadingPage(),
 };
