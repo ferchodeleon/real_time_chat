995293583ba2a0be1833ccef437128e9822dbef6
Fernando De Leon <fdeleon@lacadenaretail.com>
2023-03-07 17:30:19 -0500

    add routes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,1 +1,13 @@
-final Map<String, dynamic> appRoutes = {};
+import 'package:flutter_real_time_chat/pages/chat_page.dart';
+import 'package:flutter_real_time_chat/pages/loading_page.dart';
+import 'package:flutter_real_time_chat/pages/login_page.dart';
+import 'package:flutter_real_time_chat/pages/register_page.dart';
+import 'package:flutter_real_time_chat/pages/users_page.dart';
+
+final Map<String, dynamic> appRoutes = {
+  'usuarios': (_) => UsersPage(),
+  'chat': (_) => ChatPage(),
+  'login': (_) => LoginPage(),
+  'register': (_) => RegisterPage(),
+  'loading': (_) => LoadingPage(),
+};
