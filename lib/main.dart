ddc460aee25c347bb3dfdd79b7a0b9e22187d10d
Fernando De Leon <fdeleon@lacadenaretail.com>
2023-03-07 17:39:39 -0500

    add structure of page main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_real_time_chat/routes/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,15 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Main'),
-        ),
-      ),
+      title: 'Chat App',
+      initialRoute: 'loading',
+      routes: appRoutes,
     );
   }
 }
