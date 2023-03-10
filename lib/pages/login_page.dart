651d45301781960b877faed84b3fc998d9de7cf2
Fernando De Leon <fdeleon@lacadenaretail.com>
2023-03-10 11:08:38 -0500

    Create page login pages

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,13 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_real_time_chat/widgets/custom_input.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login page'),
+    return Scaffold(
+      backgroundColor: const Color(0xfff2f2f2),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              _Logo(),
+              _Form(),
+              _Labels(),
+              Text(
+                'Terminos y condiciones de uso',
+                style: TextStyle(fontWeight: FontWeight.w200),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  const _Logo();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.only(top: 50),
+        width: 170,
+        child: Column(
+          children: const [
+            Image(
+              image: AssetImage('assets/chat.png'),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Messenger',
+              style: TextStyle(fontSize: 30),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Form extends StatefulWidget {
+  const _Form();
+
+  @override
+  State<_Form> createState() => __FormState();
+}
+
+class __FormState extends State<_Form> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 40),
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: [
+          CustomInput(),
+          CustomInput(),
+          const TextField(),
+          //TODO: Crear botón
+          // ElevatedButton(
+          //   onPressed: () {},
+          //   child: const Text('Ingresar'),
+          // ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Labels extends StatelessWidget {
+  const _Labels();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Column(
+        children: [
+          const Text(
+            '¿No tienes cuenta?',
+            style: TextStyle(
+              color: Colors.black54,
+              fontSize: 15,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            '¡Crea una ahora!',
+            style: TextStyle(
+              color: Colors.blue[600],
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
       ),
     );
   }
