6d2e1ffeb09f5d02727365cc78ccfd27322645d2
Fernando De Leon <fdeleon@lacadenaretail.com>
2023-03-10 10:57:30 -0500

    Create custom input for the login page

---
+++ lib/widgets/custom_input.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CustomInput extends StatelessWidget {
+  const CustomInput({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      padding: const EdgeInsets.only(
+        top: 5,
+        left: 5,
+        bottom: 5,
+        right: 20,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            offset: const Offset(0, 5),
+            blurRadius: 5,
+          ),
+        ],
+      ),
+      child: const TextField(
+        autocorrect: false,
+        keyboardType: TextInputType.emailAddress,
+        // obscureText: true,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail_outline),
+          focusedBorder: InputBorder.none,
+          border: InputBorder.none,
+          hintText: 'Email',
+        ),
+      ),
+    );
+  }
+}
