3ca0cc6e94ca234438f51a2331aa3cc0ab98642e
Fernando De Leon <fdeleon@lacadenaretail.com>
2023-03-14 16:22:15 -0500

    create a basic chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -5,9 +5,46 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('chat page'),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 1,
+        backgroundColor: Colors.white,
+        title: Column(
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.blue[100],
+              maxRadius: 15,
+              child: const Text('Te', style: TextStyle(fontSize: 12)),
+            ),
+            const SizedBox(height: 3),
+            const Text(
+              'Maria',
+              style: TextStyle(color: Colors.black87, fontSize: 12),
+            )
+          ],
+        ),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                itemBuilder: (_, i) => Text('$i'),
+                physics: const BouncingScrollPhysics(),
+                reverse: true,
+              ),
+            ),
+            Divider(
+              height: 1,
+            ),
+            //TODO: Caja de texto
+            Container(
+              color: Colors.white,
+              height: 100,
+            ),
+          ],
+        ),
       ),
     );
   }
