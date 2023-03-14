bb4b9b86f746cf8776f3bf6b536e001089371496
Fernando De Leon <fdeleon@lacadenaretail.com>
2023-03-14 17:01:23 -0500

    Create chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,7 +1,18 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final _textController = TextEditingController();
+  final _focusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
@@ -41,11 +52,55 @@ class ChatPage extends StatelessWidget {
             //TODO: Caja de texto
             Container(
               color: Colors.white,
-              height: 100,
+              height: 50,
+              child: _inputChat(),
             ),
           ],
         ),
       ),
     );
+  }
+
+  Widget _inputChat() {
+    return SafeArea(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSumit,
+                onChanged: (String text) {
+                  //TODO: Cuando hay un valor, para poder postear
+                },
+                decoration:
+                    InputDecoration.collapsed(hintText: 'Enviar mensaje'),
+                focusNode: _focusNode,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: Platform.isIOS
+                  ? CupertinoButton(child: Text('Enviar'), onPressed: () {})
+                  : Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                      child: IconButton(
+                        color: Colors.blue[400],
+                        onPressed: () {},
+                        icon: const Icon(Icons.send),
+                      ),
+                    ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _handleSumit(String texto) {
+    print(texto);
+    _textController.clear();
+    _focusNode.requestFocus();
   }
 }
