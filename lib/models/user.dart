e3d6d98af14e8316c1a823564caadd45cf6b8c7d
Fernando De Leon <fdeleon@lacadenaretail.com>
2023-03-14 10:17:00 -0500

    Model of users from user_page.dart

---
+++ lib/models/user.dart
@@ -0,0 +1,13 @@
+class User {
+  User({
+    required this.online,
+    required this.email,
+    required this.name,
+    required this.uid,
+  });
+
+  bool online;
+  String email;
+  String name;
+  String uid;
+}
